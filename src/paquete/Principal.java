d168bf2e05271a1e88270694a1ed08d7faa5dcca
Iñaki <124537971+Imansortega@users.noreply.github.com>
2023-03-31 16:14:39 -0300

    Commit previo a upload a GitHub, cambios

--- src/paquete/Principal.java
+++ src/paquete/Principal.java
@@ -3,18 +3,17 @@ package paquete;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("\nHola mundo, carajo !!\n");
-//		Class c = java.util.ArrayList.class;
-//		String className = c.getName();
-//		System.out.println("The fully-qualified name of the class is: " + className);
-
+		System.out.println("\nEn esta clase se dan los datos de qualified name, package y classpath\n");
 		
+		@SuppressWarnings("rawtypes")
+		Class c = java.util.ArrayList.class;
+		String className = c.getName();
+		System.out.println("The fully-qualified name of the class is: " + className);
 		System.out.println("El \"fully-qualified name\" de la clase es: ---> " + Principal.class);
-		System.out.println("El nombre del package es:                 ---> " + Principal.class.getPackageName());
-
-		
+		System.out.println("El nombre del package es:                 ---> " + Principal.class.getPackageName());	
 		System.out.println("El classpath es:                          ---> " + System.getProperty("java.class.path"));
 		System.out.println("----------------------------------------------");
+		
 	}
 
 }
