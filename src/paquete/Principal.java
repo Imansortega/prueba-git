86cc470a4de4d73262092814f279a94b98342830
Iñaki <124537971+Imansortega@users.noreply.github.com>
2023-03-31 15:51:45 -0300

    Hago commit antes de pasar a branch "cambios"

--- src/paquete/Principal.java
+++ src/paquete/Principal.java
@@ -3,12 +3,18 @@ package paquete;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo, carajo !!");
-// Cambio en localcito...
-/* Nada de cambios cabrón !!!
-*  Si...
-*  27-03-2023.
-*/
+		System.out.println("\nHola mundo, carajo !!\n");
+//		Class c = java.util.ArrayList.class;
+//		String className = c.getName();
+//		System.out.println("The fully-qualified name of the class is: " + className);
+
+		
+		System.out.println("El \"fully-qualified name\" de la clase es: ---> " + Principal.class);
+		System.out.println("El nombre del package es:                 ---> " + Principal.class.getPackageName());
+
+		
+		System.out.println("El classpath es:                          ---> " + System.getProperty("java.class.path"));
+		System.out.println("----------------------------------------------");
 	}
 
 }
