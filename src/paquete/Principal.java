63565c6fe19624a011d878093fee2f69cbf1a4aa
Iñaki <124537971+Imansortega@users.noreply.github.com>
2023-03-30 23:22:23 -0300

    Cambio en local. Agrego ---> // Cambio en local.

--- src/paquete/Principal.java
+++ src/paquete/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, carajo !!");
+// Cambio en local.
 /* Nada de cambios cabrón !!!
 *  Si...
 *  27-03-2023.
