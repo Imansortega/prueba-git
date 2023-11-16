3f0983de08481ab915b41a46994e995f678ee81d
Iñaki <124537971+Imansortega@users.noreply.github.com>
2023-11-16 17:07:45 -0300

    Change-Id: I0000000000000000000000000000000000000000

---
+++ README.md
@@ -0,0 +1,2 @@
+# prueba-git
+2023-04-08 Prueba
---
+++ src/paquete/Principal.java
@@ -0,0 +1,38 @@
+package paquete;
+
+import utilidades.Sistema;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+//		Principal miPrincipal = new Principal();
+		System.out.println("caca");
+		//Sistema.sistemaEntorno();
+//		System.out.println("\n- Variables de entorrno -\n");
+////		Class c = java.util.ArrayList.class;
+////		String className = c.getName();
+////		System.out.println("The fully-qualified name of the class is: " + className);
+//
+//		
+//		System.out.println("El \"fully-qualified name\" de la clase es:   ---> " + Principal.class);
+//		System.out.println("El nombre del package es:                   ---> " + Principal.class.getPackageName());
+//		System.out.println("El classpath es:                            ---> " + System.getProperty("java.class.path"));
+//		System.out.println("JDK version es:                             ---> " + System.getProperty("java.version"));
+//		System.out.println("JDK Installation directory:                 ---> " + System.getProperty("java.home"));
+
+		System.out.println("\nEn esta clase se dan los datos de qualified name, package y classpath\n");
+		
+		@SuppressWarnings("rawtypes")
+		Class c = java.util.ArrayList.class;
+		String className = c.getName();
+		System.out.println("The fully-qualified name of the class is: " + className);
+		System.out.println("El \"fully-qualified name\" de la clase es: ---> " + Principal.class);
+		System.out.println("El nombre del package es:                 ---> " + Principal.class.getPackageName());	
+		System.out.println("El classpath es:                          ---> " + System.getProperty("java.class.path"));
+		System.out.println("----------------------------------------------");
+		//
+	}
+
+}
